3172ba8c2503a3d69bb4c470f04842c0121fd6dc
Tomerporian <57691643+Tomerporian@users.noreply.github.com>
2023-04-30 15:05:28 +0300

    Update vggnet.py
    Original code from https://github.com/kuangliu/pytorch-cifar/blob/master/models/vgg.py

--- src/models/tiny_models/vggnet.py
+++ src/models/tiny_models/vggnet.py
@@ -1,91 +1,52 @@
-"""Code source: https://github.com/meliketoy/wide-resnet.pytorch"""
+'''VGG11/13/16/19 in Pytorch.
+Taken from https://github.com/kuangliu/pytorch-cifar/blob/master/models/vgg.py
+'''
 import torch
 import torch.nn as nn
-from torch.nn import init
-from torch.autograd import Variable
-import numpy as np
 
 
-def conv_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        init.xavier_uniform(m.weight, gain=np.sqrt(2))
-        init.constant(m.bias, 0)
-
-
-def cfg(depth):
-    depth_lst = [11, 13, 16, 19]
-    assert (depth in depth_lst), "Error : VGGnet depth should be either 11, 13, 16, 19"
-    cf_dict = {
-        '11': [
-            64, 'mp',
-            128, 'mp',
-            256, 256, 'mp',
-            512, 512, 'mp',
-            512, 512, 'mp'],
-        '13': [
-            64, 64, 'mp',
-            128, 128, 'mp',
-            256, 256, 'mp',
-            512, 512, 'mp',
-            512, 512, 'mp'
-        ],
-        '16': [
-            64, 64, 'mp',
-            128, 128, 'mp',
-            256, 256, 256, 'mp',
-            512, 512, 512, 'mp',
-            512, 512, 512, 'mp'
-        ],
-        '19': [
-            64, 64, 'mp',
-            128, 128, 'mp',
-            256, 256, 256, 256, 'mp',
-            512, 512, 512, 512, 'mp',
-            512, 512, 512, 512, 'mp'
-        ],
-    }
-
-    return cf_dict[str(depth)]
-
-
-def conv3x3(in_planes, out_planes, stride=1):
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=True)
+cfg = {
+    11: [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    13: [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    16: [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    19: [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
 
 
 class VGG(nn.Module):
-    def __init__(self, depth, num_classes):
+    def __init__(self, depth, num_classes=10):
         super(VGG, self).__init__()
-        self.features = self._make_layers(cfg(depth))
-        self.linear = nn.Linear(512, num_classes)
+        self.features = self._make_layers(cfg[depth])
+        self.classifier = nn.Linear(512, num_classes)
 
     def forward(self, x):
         out = self.features(x)
         out = out.view(out.size(0), -1)
-        out = self.linear(out)
-
+        out = self.classifier(out)
         return out
 
     def _make_layers(self, cfg):
         layers = []
-        in_planes = 3
-
+        in_channels = 3
         for x in cfg:
-            if x == 'mp':
+            if x == 'M':
                 layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
             else:
-                layers += [conv3x3(in_planes, x), nn.BatchNorm2d(x), nn.ReLU(inplace=True)]
-                in_planes = x
-
-        # After cfg convolution
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU(inplace=True)]
+                in_channels = x
         layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
         return nn.Sequential(*layers)
-
+    
     def get_classifier(self):
-        return self.linear
+        return self.classifier
 
 
-if __name__ == "__main__":
-    net = VGG(16, 10)
-    y = net(Variable(torch.randn(1, 3, 32, 32)))
+def test():
+    net = VGG('VGG11')
+    x = torch.randn(2,3,32,32)
+    y = net(x)
     print(y.size())
+
+# test()
